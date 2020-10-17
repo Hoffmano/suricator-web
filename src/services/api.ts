dad39a63fb14c378875d3afd67f580065060528f
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-10-17 16:35:47 -0300

    fix: coloca base_url como variavel de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const api = axios.create({
-	baseURL: "http://localhost:3333",
+	baseURL: process.env.BASE_URL,
 });
 
 export default api;
