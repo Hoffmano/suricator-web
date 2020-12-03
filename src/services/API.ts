76eccfeeb3b34e57dc63437b9e03bd7c0c28bee5
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-12-03 14:54:45 -0300

    Update API.ts

--- src/services/API.ts
+++ src/services/API.ts
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-
 export const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL_API,
   headers: {
